01b78a9d0f2ccc9f474e0b8d9eaaac01a6e2bf0c
Saarthak <saarthakgupta08@gmail.com>
2019-03-29 03:39:22 +0530

    Added service classes

---
+++ app/src/main/java/com/example/popularmovies/service/MovieDataService.java
@@ -0,0 +1,14 @@
+package com.example.popularmovies.service;
+
+import com.example.popularmovies.model.Movie;
+import com.example.popularmovies.model.MovieDBResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieDataService {
+
+    @GET("movie/popular")
+    Call<MovieDBResponse> getPopularMovies(@Query("api_key")String apiKey);
+}
---
+++ app/src/main/java/com/example/popularmovies/service/RetrofitInstance.java
@@ -0,0 +1,18 @@
+package com.example.popularmovies.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit=null;
+    private static String BASE_URL="https://api.themoviedb.org/3/";
+    private static MovieDataService getService()
+    {
+            if(retrofit==null)
+            {
+                retrofit=new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+            }
+            return retrofit.create(MovieDataService.class);
+    }
+}
