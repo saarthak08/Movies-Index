b340f14f00720e298040a463d3b4766ab0ce1d53
Saarthak <saarthakgupta08@gmail.com>
2019-03-29 12:12:17 +0530

    Added swipetorefresh layout

--- app/src/main/java/com/example/popularmovies/view/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/view/MainActivity.java
@@ -1,13 +1,18 @@
-package com.example.popularmovies;
+package com.example.popularmovies.view;
 
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.widget.Toast;
 
+import com.example.popularmovies.BuildConfig;
+import com.example.popularmovies.R;
 import com.example.popularmovies.adapter.MoviesAdapter;
 import com.example.popularmovies.model.Movie;
 import com.example.popularmovies.model.MovieDBResponse;
@@ -21,15 +26,26 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private String ApiKey=BuildConfig.ApiKey;
+    private String ApiKey= BuildConfig.ApiKey;
     ArrayList<Movie> movies=new ArrayList<>();
     private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getData();
+        swipeRefreshLayout=findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE, Color.DKGRAY, Color.RED,Color.GREEN,Color.MAGENTA,Color.BLACK,Color.CYAN);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData();
+            }
+        });
+
+
     }
 
     private void getData() {
@@ -48,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<MovieDBResponse> call, Throwable t) {
-
+                Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                },5000);
             }
         });
 
@@ -62,9 +84,10 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,2));
         }
         else
-            recyclerView.setLayoutManager((new GridLayoutManager(MainActivity.this,4)));
+        {recyclerView.setLayoutManager((new GridLayoutManager(MainActivity.this,4)));}
         recyclerView.setAdapter(moviesAdapter);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         moviesAdapter.notifyDataSetChanged();
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
