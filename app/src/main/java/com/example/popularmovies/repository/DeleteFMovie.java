1e3b677d6de71a0d0ed43f81275e4a4962b40bbd
Saarthak <saarthakgupta08@gmail.com>
2019-03-29 16:02:23 +0530

    Added other Room Services

---
+++ app/src/main/java/com/example/popularmovies/repository/DeleteFMovie.java
@@ -0,0 +1,20 @@
+package com.example.popularmovies.repository;
+
+import android.os.AsyncTask;
+
+import com.example.popularmovies.adapter.db.FavouriteMoviesDAO;
+import com.example.popularmovies.model.Movie;
+
+public class DeleteFMovie extends AsyncTask<Movie, Void,Void> {
+    private FavouriteMoviesDAO favouriteMoviesDAO;
+
+    public DeleteFMovie(FavouriteMoviesDAO favouriteMoviesDAO) {
+        this.favouriteMoviesDAO = favouriteMoviesDAO;
+    }
+
+    @Override
+    protected Void doInBackground(Movie... movies) {
+        favouriteMoviesDAO.deleteFMovie(movies[0]);
+        return null;
+    }
+}
