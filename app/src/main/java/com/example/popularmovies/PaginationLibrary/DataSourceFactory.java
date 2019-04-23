c8a856970a369866b56d8dd39a39e8dd821a76b1
Saarthak <saarthakgupta08@gmail.com>
2019-04-23 23:51:40 +0530

    Modified MainViewModel class for Paging

--- app/src/main/java/com/example/popularmovies/PaginationLibrary/DataSourceFactory.java
+++ app/src/main/java/com/example/popularmovies/PaginationLibrary/DataSourceFactory.java
@@ -2,25 +2,27 @@ package com.example.popularmovies.PaginationLibrary;
 
 import android.app.Application;
 
+import com.example.popularmovies.model.Movie;
 import com.example.popularmovies.service.MovieDataService;
 
 import androidx.lifecycle.MutableLiveData;
 
-public class DataSourceFactory {
+public class DataSourceFactory extends DataSource.Factory{
     private DataSource dataSource;
     private Application application;
     private MovieDataService movieDataService;
     private MutableLiveData<DataSource> dataSourceMutableLiveData;
 
-    public DataSourceFactory(Application application, MovieDataService movieDataService) {
+    public DataSourceFactory(MovieDataService movieDataService,Application application) {
         this.application = application;
         this.movieDataService = movieDataService;
     }
 
-    public void create()
+    public DataSource create()
     {
         dataSource=new DataSource(movieDataService,application);
         dataSourceMutableLiveData.postValue(dataSource);
+        return null;
     }
 
     public MutableLiveData<DataSource> getDataSourceMutableLiveData()
--- app/src/main/java/com/example/popularmovies/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/popularmovies/viewmodel/MainViewModel.java
@@ -2,8 +2,12 @@ package com.example.popularmovies.viewmodel;
 
 import android.app.Application;
 
+import com.example.popularmovies.PaginationLibrary.DataSource;
+import com.example.popularmovies.PaginationLibrary.DataSourceFactory;
 import com.example.popularmovies.model.Movie;
 import com.example.popularmovies.repository.Repository;
+import com.example.popularmovies.service.MovieDataService;
+import com.example.popularmovies.service.RetrofitInstance;
 
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -14,12 +18,33 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
 
 public class MainViewModel extends AndroidViewModel {
     private Repository repository;
+    private LiveData<DataSource> dataSourceLiveData;
+    private Executor executor;
+    private LiveData<PagedList<Movie>> pagedListLiveData;
     public MainViewModel(@NonNull Application application) {
         super(application);
         repository=new Repository(application);
+        MovieDataService movieDataService= RetrofitInstance.getService();
+        DataSourceFactory dataSourceFactory=new DataSourceFactory(movieDataService,application);
+        dataSourceLiveData=dataSourceFactory.getDataSourceMutableLiveData();
+        PagedList.Config config=(new PagedList.Config.Builder())
+                .setEnablePlaceholders(true)
+                .setInitialLoadSizeHint(10)
+                .setPageSize(20)
+                .setPrefetchDistance(4)
+                .build();
+        executor=Executors.newFixedThreadPool(5);
+        pagedListLiveData=(new LivePagedListBuilder<Long,Movie>(dataSourceFactory,config)).setFetchExecutor(executor).build();
+    }
+
+
+    public LiveData<PagedList<Movie>> getPagedListLiveData() {
+        return pagedListLiveData;
     }
 
     public Movie getMovie(String id)
